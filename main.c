ad66433623118a24e42f2d46f11a3bab4acf3c67
Amir <amir.07hossein18@gmail.com>
2026-04-14 15:16:55 +0200

    le fichier principale

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Ce cemmentaire est sur une seule ligne
+/*
+
+ce est commentaire
+sur plusieure lignes                 même avec des tabulation
+
+
+*/
+int main(void)
+{
+    puts("Bonjour tout le monde");
+    return 0;
+}
